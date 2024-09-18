7d8cb89e8f44d4d2176d3f9de9ef1875dedbf437
Robert He <2567466856@qq.com>
2024-09-19 01:25:01 +0800

    1

--- models/history.js
+++ models/history.js
@@ -1,10 +1,11 @@
+const mongoose = require("mongoose");
 
 const historySchema = new mongoose.Schema({
-    applicantName: String,
-    applicantStudentId: String,
-    giftTitle: String,
-    giftName: String,
-    date: { type: Date, default: Date.now }
+  applicantName: String,
+  applicantStudentId: String,
+  giftTitle: String,
+  giftName: String,
+  date: { type: Date, default: Date.now },
 });
 
-const History = mongoose.model('History', historySchema);
+module.exports = mongoose.model("Histories", historySchema);
